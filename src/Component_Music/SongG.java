3856c332daf198ba102ca4c501287e10b111211e
62010710 <62010710@DESKTOP-L1RAVSR>
2020-04-10 21:11:48 +0700

    "Create account eiei"

---
+++ src/Component_Music/SongG.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Component_Music;
+
+/**
+ *
+ * @author 62010710
+ */
+public class SongG {
+    private String aikwaipop;
+}
